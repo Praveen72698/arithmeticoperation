f09cec34133609a554a587702998159ec1bd9cd2
kamalesh.B <82257435+kamalesh20-hacker@users.noreply.github.com>
2021-04-14 11:40:08 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -5,3 +5,10 @@ print("Subraction=",a-b)
 print("Multiplication=",a*b)
 print("Division=",a/b)     
       
+output:
+Enter the Number A:20
+Enter the Number B:10
+Addition= 30
+Subraction= 10
+Multiplication= 200
+Division= 2.0
